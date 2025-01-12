8ae5b216484f6a856497bf2fa45060607b0d3d76
Simone3838 <simone.paolini14@gmail.com>
2025-01-12 19:59:54 +0100

    Update boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,22 +1,38 @@
 ; boot/boot.asm
-BITS 16
-ORG 0x7C00
+BITS 32
+ORG 0x100000
 
+; Multiboot header
+multiboot_header:
+    dd 0x1BADB002   ; magic number
+    dd 0x00010003   ; flags
+    dd - (0x1BADB002 + 0x00010003) ; checksum
+
+; Entry point for the bootloader
 start:
-    ; Set up stack
-    cli
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7C00
+    ; Set up the stack
+    mov esp, 0x9C000
 
-    ; Load the kernel
-    mov si, hello_msg
+    ; Clear the screen
+    call clear_screen
+
+    ; Print a message
+    mov si, welcome_msg
     call print_string
 
-    ; Hang
-    jmp $
+    ; Jump to the kernel's entry point
+    call kernel_main
+
+hang:
+    hlt
+    jmp hang
+
+clear_screen:
+    mov edi, 0xB8000
+    mov ecx, 2000
+    mov eax, 0x07200720
+    rep stosd
+    ret
 
 print_string:
     mov ah, 0x0E
@@ -29,7 +45,7 @@ print_string:
 .done:
     ret
 
-hello_msg db 'Loading DemoOS...', 0
+welcome_msg db 'Welcome to DemoOS', 0
 
 times 510-($-$$) db 0
 dw 0xAA55
