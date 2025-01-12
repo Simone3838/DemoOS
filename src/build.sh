91a01a1a1eebe7c9175f4b509508ff03c2779abd
Simone3838 <simone.paolini14@gmail.com>
2025-01-12 20:01:35 +0100

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -2,14 +2,28 @@
 
 set -e
 
+# Directories
+BOOT_DIR="boot"
+KERNEL_DIR="kernel"
+ISO_DIR="isodir"
+
 # Assemble the bootloader
-nasm -f bin boot/boot.asm -o boot/boot.bin
+nasm -f bin $BOOT_DIR/boot.asm -o $BOOT_DIR/boot.bin
 
 # Compile the kernel
-gcc -ffreestanding -c kernel/kernel.c -o kernel/kernel.o
+gcc -ffreestanding -c $KERNEL_DIR/kernel.c -o $KERNEL_DIR/kernel.o
 
 # Link the kernel
-ld -o kernel/kernel.bin -Ttext 0x1000 --oformat binary kernel/kernel.o
+ld -o $KERNEL_DIR/kernel.bin -Ttext 0x1000 --oformat binary $KERNEL_DIR/kernel.o
 
-# Create the OS image
-cat boot/boot.bin kernel/kernel.bin > DemoOS.bin
+# Create the ISO directory structure
+mkdir -p $ISO_DIR/boot/grub
+cp $BOOT_DIR/boot.bin $ISO_DIR/boot/boot.bin
+cp $KERNEL_DIR/kernel.bin $ISO_DIR/boot/kernel.bin
+cp grub.cfg $ISO_DIR/boot/grub/grub.cfg
+
+# Create the bootable ISO image
+grub-mkrescue -o DemoOS.iso $ISO_DIR
+
+# Clean up
+rm -rf $ISO_DIR
